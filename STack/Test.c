6323e20fc929f7ea88d014bfd74d33a6d02f98dd
shixuzhimeng <1516446316@qq.com>
2026-01-09 22:49:23 +0800

    STack

---
+++ STack/Test.c
@@ -0,0 +1,21 @@
+#include "STack.h"
+
+int main()
+{
+    ST st;
+    STInit(&st);
+    STPush(&st,1);
+    STPush(&st,2);
+    STPush(&st,3);
+    STPush(&st,4);
+
+    while(!STEmpty(&st))
+    {
+        printf("%d",STTop(&st));
+        STPop(&st);
+    }
+    
+    STDestory(&st);
+    
+    return 0;
+}

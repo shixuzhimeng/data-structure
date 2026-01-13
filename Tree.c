7b8ddb9183d02dacf9c6951a3884a899dfd1f55e
shixuzhimeng <1516446316@qq.com>
2026-01-13 23:42:31 +0800

    Tree

---
+++ Tree.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// //树的度为n
+// #define N 5
+// struct TreeNode
+// {
+//     struct TreeNode* children[N];//指针数组
+//     int data;
+// };
+
+// //不知道度为多少
+// struct TreeNode
+// {
+//     SeqList s1;   //存的数据类型struct TreeNode*
+//     int data;
+// };
+
+
+int main()
+{
+    return 0;
+}
